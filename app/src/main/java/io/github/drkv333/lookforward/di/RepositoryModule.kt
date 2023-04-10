e6507014292cd5bfc85758dcfaebd4cb760e119b
DRKV333 <30904009+DRKV333@users.noreply.github.com>
2023-04-10 17:50:28 +0200

    Added Hilt modules

---
+++ app/src/main/java/io/github/drkv333/lookforward/di/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package io.github.drkv333.lookforward.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule
